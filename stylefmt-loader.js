92544a5896a592bfefeffd913b16dd6efd8ac0c2
Erik Hughes <erik.hughes@outlook.com>
2017-04-17 14:33:41 +0200

    Added postcss-sorting to loader

--- stylefmt-loader.js
+++ stylefmt-loader.js
@@ -3,27 +3,26 @@
 const stylefmt = require('stylefmt');
 const postcss = require('postcss');
 const scss = require('postcss-scss');
+const sorting = require('postcss-sorting');
 const loaderUtils = require("loader-utils");
 
 const fs = require('fs');
 
 module.exports = function (source) {
-
   let callback = this.async();
   let resourcePath = this.resourcePath;
   let query = loaderUtils.parseQuery(this.query);
-  let stylefmtConf;
 
-  if (query.config) {
-    stylefmtConf = stylefmt({
-      configFile: `${process.cwd()}/${query.config}`
-    })
-  } else {
-    stylefmtConf = stylefmt();
-  }
+  let stylefmtConf = stylefmt(query.stylefmtConfig ? {
+    configFile: `${process.cwd()}/${query.config}`
+  } : undefined);
+
+  let sortingConf = sorting(query.sortingConfig ?
+    require(`${process.cwd()}/${query.config}`) : {});
 
   postcss([
-    stylefmtConf
+    stylefmtConf,
+    sortingConf,
   ])
     .process(source, { syntax: scss })
     .then(function (result) {
